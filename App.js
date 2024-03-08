3a18ae6133887e8d59656de57dfe4052353fec88
Bhuvanaboston <149010119+Bhuvanaboston@users.noreply.github.com>
2024-03-08 22:11:44 +0530

    after ep-3

--- App.js
+++ App.js
@@ -1,3 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const header=React.createElement("h1",{},"Bhuvana Boston");
 const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(header);
+const jsxhead = (<>
+<h1>Helloo</h1>
+<div className="container">
+    <h2>Bhuvana Boston</h2>
+</div>
+</>);
+root.render(jsxhead);
